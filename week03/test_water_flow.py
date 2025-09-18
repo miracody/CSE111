206927a526ba23c4ff7ddbc6f9003ef8edd14aa3
Mirabel Nwanneka <memeruwa@gmail.com>
2025-09-18 09:37:11 +0100

    ok

--- week03/test_water_flow.py
+++ week03/test_water_flow.py
@@ -1,26 +1,25 @@
 from pytest import approx
-
 from waterflow import 
-    water_column_height, \
+    test_water_column_height, \
     pressure_gain_from_water_height, \
     pressure_loss_from_pipe, \
     pressure_loss_from_fittings, \
     reynolds_number, \
     pressure_loss_from_pipe_reduction
-   import pytest
+import pytest
 
-def test_water_column_height():
+def water_column_height():
     assert water_column_height(0.0, 0.0) == approx(0.0)
     assert water_column_height(0.0, 10.0) == approx(7.5)
     assert water_column_height(25.0, 0.0) == approx(25.0)
     assert water_column_height(48.3, 12.8) == approx(57.9)
 
-def test_pressure_gain_from_water_height():
+def pressure_gain_from_water_height():
     assert pressure_gain_from_water_height(0.0) == approx(0.000, abs=0.001)
     assert pressure_gain_from_water_height(30.2) == approx(295.628, abs=0.001)
     assert pressure_gain_from_water_height(50.0) == approx(489.450, abs=0.001)
 
-def test_pressure_loss_from_pipe():
+def pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.048692, 0.0, 0.018, 1.75) == approx(0.000, abs=0.001)
     assert pressure_loss_from_pipe(0.048692, 200.0, 0.000, 1.75) == approx(0.000, abs=0.001)
     assert pressure_loss_from_pipe(0.048692, 200.0, 0.018, 0.0) == approx(0.000, abs=0.001)
@@ -29,26 +28,26 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.28687, 1000.0, 0.013, 1.65) == approx(-61.576, abs=0.001)
     assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
 
-def test_pressure_loss_from_fittings():
+def pressure_loss_from_fittings():
     assert pressure_loss_from_fittings(0.0, 3) == approx(0.000, abs=0.001)
     assert pressure_loss_from_fittings(1.65, 0) == approx(0.000, abs=0.001)
     assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
     assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
     assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
 
-def test_reynolds_number():
+def reynolds_number():
     assert reynolds_number(0.048692, 0.0) == approx(0, abs=1)
     assert reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
     assert reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
     assert reynolds_number(0.28687, 1.65) == approx(471729, abs=1)
     assert reynolds_number(0.28687, 1.75) == approx(500318, abs=1)
 
-def test_pressure_loss_from_pipe_reduction():
+def pressure_loss_from_pipe_reduction():
     assert pressure_loss_from_pipe_reduction(0.28687, 0.0, 1, 0.048692) == approx(0.000, abs=0.001)
     assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, abs=0.001)
     assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, abs=0.001)
 
-def test_kpa_to_psi():
+def kpa_to_psi():
     assert kpa_to_psi(0.0) == approx(0.0, abs=0.001)
     assert kpa_to_psi(158.7) == approx(23.015, abs=0.001)
     assert kpa_to_psi(295.628) == approx(42.851, abs=0.001)
